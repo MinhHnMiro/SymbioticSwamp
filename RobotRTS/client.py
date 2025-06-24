701a1843a5af507ac63a0a7a845d784f0d739450
MinhHnMiro <119595286+MinhHnMiro@users.noreply.github.com>
2025-06-24 11:18:42 +0300

    Add files via upload

--- RobotRTS/client.py
+++ RobotRTS/client.py
@@ -16,19 +16,6 @@ ADDR = (SERVER, PORT)
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect(ADDR)
 
-class Sprite:
-    def __init__(self, x, y, shape, color, name='Dummy', size=[1, 1, None], heading=90, team='s', give_energy=50): # team: s=self, f=friendly, e=enemy, n=neatral
-        self.x = x
-        self.y = y
-        self.heading = heading
-        self.shape = shape
-        self.color = color
-        self.size = size
-        self.name = name
-        self.energy = 100
-        self.team = team
-        self.surface = pygame.Surface(self.size)
-
 def send(msg):
     message = msg.encode(FORMAT)
     msg_length = len(message)
@@ -38,12 +25,54 @@ def send(msg):
     client.send(message)
     return client.recv(HEADER).decode(FORMAT)
 
+lentities = {}
+
+surface = pygame.Surface((200, 200))
+
+text = []
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            print(send(DISCONNECT_MESSAGE))
+            send(DISCONNECT_MESSAGE)
             pygame.quit()
             exit()
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_w:
+                text += 'w'
+            if event.key == pygame.K_a:
+                text += 'a'
+            if event.key == pygame.K_s:
+                text += 's'
+            if event.key == pygame.K_d:
+                text += 'd'
+                
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_w:
+                text.remove('w')
+            if event.key == pygame.K_a:
+                text.remove('a')
+            if event.key == pygame.K_s:
+                text.remove('s')
+            if event.key == pygame.K_d:
+                text.remove('d')
 
+    print(text)
+    entities = send(''.join(text))
+    entit = entities.split('\t')
+    enti = []
+    # Fix client handling Server info with multiple Objects
+    '''for e in entit:
+        enti.append(e.split(','))
+        ent = enti[0].split('.')
+        en = []
+        for e in ent:
+            en.append(int(e))
+        en = tuple(en)
+        
+        surface.fill(enti[1])
+        wn.blit(surface, en)
+'''
     pygame.display.update()
     clock.tick(60)
+
+    wn.fill('black')
--- RobotRTS/server.py
+++ RobotRTS/server.py
@@ -1,5 +1,5 @@
 import socket
-import threading
+import threading, random
 
 HEADER = 64
 PORT = 5056
@@ -12,10 +12,32 @@ DISCONNECT_MESSAGE = '!DISCONNECT!'
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind(ADDR)
 
+pwc = {}
+class Sprite:
+    def __init__(self, x, y, name='Dummy', size=(20,20), heading=90, team='s', give_energy=50, types='Dummy'): # team: s=self, f=friendly, e=enemy, n=neatral
+        self.x = x
+        self.y = y
+        self.heading = heading
+        self.size = size
+        self.name = name
+        self.energy = 100
+        self.team = team
+        self.give_energy = give_energy
+        self.type = types
+        
+all_entities = []
+all_entities.append(Sprite(0,0))
 
-def handle_client(conn, addr):
+def find_key(tar):
+    d = {'a': 1, 'b': 2, 'c': 2}
+    tar = tar
+    keys = [key for key, val in d.items() if val == tar]
+    return keys
+
+def handle_client(conn, addr, conn_num):
     print(f'[NEW CONNECTION] {addr} conneced')
     connected = True
+    pwc[conn_num] = {'xy': [random.randint(-50, 50), random.randint(-50, 50)]}
     while connected:
         msg_length = conn.recv(HEADER).decode(FORMAT)
         if msg_length:
@@ -23,17 +45,35 @@ def handle_client(conn, addr):
             msg = conn.recv(msg_length).decode(FORMAT)
             if msg == DISCONNECT_MESSAGE:
                 connected = False
-            conn.send('Message recieved'.encode(FORMAT))
+            msg = list(msg)
+            for ms in msg:
+                if ms == 'w':
+                    pwc[conn_num]['xy'][1] += 10
+                if ms == 'a':
+                    pwc[conn_num]['xy'][0] += 10
+                if ms == 's':
+                    pwc[conn_num]['xy'][1] -= 10
+                if ms == 'd':
+                    pwc[conn_num]['xy'][0] -= 10
+            text = ''
+            text += str(50-pwc[conn_num]['xy'][0]) + '.' + str(50 - pwc[conn_num]['xy'][1]) + ',green,200.200\t'
+            text += str(200-pwc[conn_num]['xy'][0]) + '.' + str(200 - pwc[conn_num]['xy'][1]) + ',red,200.200'
+            conn.send(text.encode(FORMAT))
+            
     conn.close()
+    print(f'Connection {addr} terminated.')
+    print('\n[ACTIVE CONNECTIONS] ' + str(threading.active_count() - 2))
 
 def start():
     server.listen()
     print('[LISTENING] Server is listening on ' + SERVER)
+    i = 0
     while True:
         conn, addr = server.accept()
-        thread = threading.Thread(target=handle_client, args=(conn, addr))
+        thread = threading.Thread(target=handle_client, args=(conn, addr, (i)))
         thread.start()
         print('\n[ACTIVE CONNECTIONS] ' + str(threading.active_count() - 1))
+        i += 1
 
 print('[STARTING] Server is starting...')
 start()
